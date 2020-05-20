9870044d4d175078e5be49f3e4467ed83166b631
prashant-masalagames <prashant@masalagames.com>
2020-05-20 23:44:41 +0530

    Updated back button callback

--- prashantsingh/UIFramework/Scripts/UI/View/ScreenView.cs
+++ prashantsingh/UIFramework/Scripts/UI/View/ScreenView.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 namespace Prashant
 {
@@ -17,15 +18,48 @@ namespace Prashant
         public override void OnScreenLoaded()
         {
             base.OnScreenLoaded();
+            StopEnumerators();
+            E_CheckingForBack = CheckingForBack();
+            StartCoroutine(E_CheckingForBack);
             _raycaster.enabled = true;
-            // Debug.Log("Screen Loaded");
         }
 
+        void StopEnumerators()
+        {
+            if (E_CheckingForBack != null)
+            {
+                StopCoroutine(E_CheckingForBack);
+                E_CheckingForBack = null;
+            }
+        }
+
+        public override void OnScreenHideCalled()
+        {
+            base.OnScreenHideCalled();
+            StopEnumerators();
+        }
         public override void OnScreenHidden()
         {
             base.OnScreenHidden();
             _raycaster.enabled = false;
-            // Debug.Log("Screen Hidden");
+        }
+
+        IEnumerator E_CheckingForBack;
+        IEnumerator CheckingForBack()
+        {
+            while (true)
+            {
+                if (Input.GetKeyDown(KeyCode.Escape))
+                {
+                    OnBackPressed();
+                }
+                yield return null;
+            }
+        }
+
+        public virtual void OnBackPressed()
+        {
+
         }
 
         public bool isCanvasActive()
